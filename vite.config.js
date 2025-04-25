e8f1f4386222506ad824f3ddaec38c73648ecd7c
Yura Prokopiv <prokopivyura@gmail.com>
2025-04-26 01:20:45 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/FlyingYura/SportRent-website-hosting', // ← важливо: назва репозиторію
+
   include: ['firebase/app', 'firebase/auth', 'react-firebase-hooks/auth']
 })
